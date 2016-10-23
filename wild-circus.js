f3860a611c86d52da73d51b5788753dc388c6a81
Wild-Circus-international <amanda.taddei@hotmail.fr>
2016-10-23 23:57:05 +0200

    Add files via upload

---
+++ wild-circus.js
@@ -0,0 +1,18 @@
+
+$( document ).ready(function() {
+
+
+        var win = $(window),
+            $box = $('.box'),
+            $imageCircus = $('.circus'),
+            item = $('.area'),
+            current = $('.inView'),
+            scroller = $('.outer');
+        $(function () {
+
+            $imageCircus.click(function () {
+                $imageCircus.wiggle(4);
+            });
+            
+        });
+});
